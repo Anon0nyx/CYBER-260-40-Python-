868b3944efb64f71725cfd9b8ac7c83c9677f6e4
terminal-is-fun <dforkey@uvm.edu>
2021-10-03 20:29:43 -0400

    Moving assignment over to windows machine

--- fifth_assignment/app.py
+++ fifth_assignment/app.py
@@ -3,14 +3,33 @@ import sys
 import os
 
 def main():
-	keyName = "userInput"
+	# Variable assignment
 	userValue = str(input("Enter a value for the registry: "))
+	now = datetime.now()
+	time = now.strftime("%H:%M:%S")
+	cwd = os.getcwd()
 
-	key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, "Software\\" + keyName)
-	winreg.SetValue(key, "AValue", winreg.REG_SZ, userValue)
-	response = winreg.QueryValue(key, "AValue")
 
-	print(response)
+	# Create user value key and input user value 
+	try:
+		key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, "Software\\" + "userInput")
+	except WindowsError as err:
+		print("Failed to create input key: " + err)
+	try:
+		winreg.SetValue(key, "input", winreg.REG_SZ, userValue)
+		# Using the same key as the user input, create the value time and set it to the time
+		winreg.SetValue(key, "time", winreg.REG_SZ, time)
+	except WindowsError as err:
+		print("Failed to set registry values: " + err)
+	# Create cwd key and input the cwd into this key
+	try:
+		key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, "Software\\" + "cwd")
+	except WindowsError as err:
+		print("Failed to create wd key: " + err)
+	try:
+		winreg.SetValue(key, "AValue", winreg.REG_SZ, cwd)
+	except WindowsError as err:
+		print("Failed to set registry value: " + err)
 
 if __name__ == "__main__":
 	main()

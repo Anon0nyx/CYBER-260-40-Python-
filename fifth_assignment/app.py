3a0ed40ac0852b7466e662c458374cefc79a66d0
terminal-is-fun <dforkey@uvm.edu>
2021-10-03 20:13:41 -0400

    Working on fifth assignment

--- fifth_assignment/app.py
+++ fifth_assignment/app.py
@@ -1,6 +1,16 @@
-import time
+import winreg
+import sys
+import os
 
 def main():
+	keyName = "userInput"
+	userValue = str(input("Enter a value for the registry: "))
+
+	key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, "Software\\" + keyName)
+	winreg.SetValue(key, "AValue", winreg.REG_SZ, userValue)
+	response = winreg.QueryValue(key, "AValue")
+
+	print(response)
 
 if __name__ == "__main__":
 	main()
